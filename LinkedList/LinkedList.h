f30d8b7110563ddb0213b7a7fcb1015658eb3ccb
Mikael <mikikusmikikus@gmail.com>
2023-01-16 22:53:56 +0300

    master Shrinked class options to push and pop options

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -23,22 +23,17 @@ public:
 	};
 
 	Node* mFirst = nullptr;
-	Node* mLast = nullptr;
-
-	unsigned long mSize;
 
 public:
-
 	LinkedList();
 
 	void push_back(const T& value);
-	T getFirst() const;
+	T pop_back();
 	bool isEmpty() const;
-
 	std::string toString() const;
 
-private:
-	friend std::ostream& operator<<(std::ostream& os, LinkedList<T>& list)
+public:
+	friend std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list)
 	{
 		if (list.isEmpty())
 			return os << std::string();
@@ -59,45 +54,106 @@ private:
 
 		return os;
 	}
+
+public:
+	class LinkedListException : public std::runtime_error {
+	private:
+		std::string mMessage;
+
+	public:
+
+		LinkedListException() : std::runtime_error("List error.") {}
+		LinkedListException(std::string message) : std::runtime_error(message.c_str())
+		{
+			mMessage = message;
+		}
+
+		std::string error()
+		{
+			return (mMessage.empty()) ? "List error." : mMessage;
+		}
+	};
 };
 
 template<typename T>
 inline LinkedList<T>::LinkedList()
-	: mFirst(nullptr),
-	mLast(nullptr),
-	mSize(0)
+	: mFirst(nullptr)
 {
 }
 
 template<typename T>
 inline void LinkedList<T>::push_back(const T& value)
 {
-	// Creating a node
+	// Create a node
 	Node* node = new Node
 	{
 		value,
 		nullptr
 	};
 
-	// The first node is the last if the list is empty
+	// If the list is empty - new node becomes the first one
 	if (isEmpty())
 	{
-		mLast = mFirst = node;
+		mFirst = node;
 		return;
 	}
 
-	// Last existed node points to the new created node
-	// and then new becomes the last
-	mLast->next = node;
-	mLast = node;
-	// Increasing the size
-	++mSize;
+	// And if it's not - find node with node->next is pointing to nullptr,
+	// that node is the last node we need. 
+	// When found, we can assign our new created node to it
+	Node* nextNode = mFirst;
+	while (nextNode)
+	{
+		if (nextNode->next)
+		{
+			nextNode = nextNode->next;
+			continue;
+		}
+
+		nextNode->next = node;
+		return;
+	}
 }
 
 template<typename T>
-inline T LinkedList<T>::getFirst() const
+inline T LinkedList<T>::pop_back()
 {
-	return (mFirst) ? mFirst->value : NULL;
+	if (isEmpty())
+	{
+		try {
+			throw LinkedListException("Error! The list is empty.");
+		}
+		catch (LinkedListException exception) {
+			std::cerr << exception.error();
+			exit(1);
+		}
+	}
+
+	// The process is similar to the one I described in 'push_back' method,
+	// but with the difference that now we need the value of the last node
+	// (and also we need a double pointer, so we can change the actual value)
+	T lastValue = T();
+	Node** nextNode = &mFirst;
+	while (nextNode)
+	{
+		if ((*nextNode)->next)
+		{
+			nextNode = &((*nextNode)->next);
+			continue;
+		}
+
+		// Get the last value
+		lastValue = (*nextNode)->value;
+
+		// Get rid of the last node
+		delete* nextNode;
+		*nextNode = nullptr;
+
+		// Decrease the size and return the value was found
+		break;
+	}
+
+	return lastValue;
 }
 
 template<typename T>
